ba02f1cd2d111b71bd7be131678d4d0023899f6f
SonAIengine <sonsj97@gmail.com>
2025-09-07 14:56:56 +0900

    상세페이지, 모달

---
+++ src/app/(main)/purchases/list/detail/[id]/DetailModalTrigger.jsx
@@ -0,0 +1,26 @@
+"use client";
+import React, { useState } from "react";
+import DetailModal from "../../../../../../components/modal/DetailModal";
+
+export default function DetailModalTrigger() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const handlePrint = () => {
+    // 실제 인쇄 기능은 필요에 따라 구현
+    window.print();
+  };
+
+  return (
+    <>
+      <button type="button" className="btn btn--light" onClick={handleOpen}>
+        <span className="ico ico--print"></span>
+        인쇄
+      </button>
+      <DetailModal open={open} onClose={handleClose} onPrint={handlePrint} />
+    </>
+  );
+}
+
+
