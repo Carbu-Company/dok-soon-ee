bd5f30fe9ee7d3bba1384c6199dad3f1457e509a
SonAIengine <sonsj97@hanyang.ac.kr>
2025-10-12 14:37:16 +0900

    매수고객 정보 전달

--- src/app/(main)/car-sell/register/RegPage.js
+++ src/app/(main)/car-sell/register/RegPage.js
@@ -367,132 +367,132 @@ export default function SalesRegisterPage({
 
     setLoading(true);
     setError(null);
+    //console.log('buyerCustomers', buyerCustomers);    // 매수고객 정보
+    // console.log('dealerId', dealerId);    // 매입딜러 ID
+    // console.log('carKndCd', carKndCd);    // 차량 종류 코드
+    // console.log('evdcCd', evdcCd);    // 증빙종류 코드
+    // console.log('carNm', carNm);    // 차량명
 
-    console.log('dealerId', dealerId);    // 매입딜러 ID
-    console.log('carKndCd', carKndCd);    // 차량 종류 코드
-    console.log('evdcCd', evdcCd);    // 증빙종류 코드
-    console.log('carNm', carNm);    // 차량명
-
-    console.log('prsnSctCd', prsnSctCd);    // 제시구분 코드
-    console.log('purAmt', purAmt);    // 매입금액
-    console.log('purSupPrc', purSupPrc);    // 공급가액
-    console.log('purVat', purVat);    // 부가세    
-    console.log('carPurDt', carPurDt);    // 매입일
-    console.log('agentPurCst', agentPurCst);    // 상사매입비
-    console.log('brokerageDate', brokerageDate);    // 상사매입비 입금일
-    console.log('carRegDt', carRegDt);    // 이전일
-    console.log('txblIssuDt', txblIssuDt);    // 발행일
-    console.log('carNo', carNo);    // 차량번호(매입후)
-    console.log('purBefCarNo', purBefCarNo);    // 차량번호(매입전)
-    console.log('ownrNm', ownrNm);    // 고객명
-    console.log('ownrTpCd', ownrTpCd);    // 고객구분
-    console.log('ctshNo', ctshNo);    // 계약서번호
-    console.log('ownrSsn', ownrSsn);    // 주민(법인)등록번호
-    console.log('ownrPhon', ownrPhon);    // 연락처
-    console.log('ownrEmail', ownrEmail);    // e메일 주소
-    console.log('emailDomain', emailDomain);    // e메일 도메인
-    console.log('ownrZip', ownrZip);    // 우편번호
-    console.log('ownrAddr1', ownrAddr1);    // 주소1
-    console.log('ownrAddr2', ownrAddr2);    // 주소2
-    console.log('ownrBrno', ownrBrno);    // 사업자등록번호
-    console.log('txblRcvYn', txblRcvYn);    // 세금 납부일
-    console.log('purDesc', purDesc);    // 특이사항
-    console.log('parkingCd', parkingCd);    // 주차위치 코드
-    console.log('parkingLocationDesc', parkingLocationDesc);    // 주차위치 설명
-    console.log('fctCndcYn', fctCndcYn);    // 사실확인서
-    console.log('attachedFiles', attachedFiles);    // 관련 서류 첨부
-    console.log('parkKeyNo', parkKeyNo);    // Key번호
+    // console.log('prsnSctCd', prsnSctCd);    // 제시구분 코드
+    // console.log('purAmt', purAmt);    // 매입금액
+    // console.log('purSupPrc', purSupPrc);    // 공급가액
+    // console.log('purVat', purVat);    // 부가세    
+    // console.log('carPurDt', carPurDt);    // 매입일
+    // console.log('agentPurCst', agentPurCst);    // 상사매입비
+    // console.log('brokerageDate', brokerageDate);    // 상사매입비 입금일
+    // console.log('carRegDt', carRegDt);    // 이전일
+    // console.log('txblIssuDt', txblIssuDt);    // 발행일
+    // console.log('carNo', carNo);    // 차량번호(매입후)
+    // console.log('purBefCarNo', purBefCarNo);    // 차량번호(매입전)
+    // console.log('ownrNm', ownrNm);    // 고객명
+    // console.log('ownrTpCd', ownrTpCd);    // 고객구분
+    // console.log('ctshNo', ctshNo);    // 계약서번호
+    // console.log('ownrSsn', ownrSsn);    // 주민(법인)등록번호
+    // console.log('ownrPhon', ownrPhon);    // 연락처
+    // console.log('ownrEmail', ownrEmail);    // e메일 주소
+    // console.log('emailDomain', emailDomain);    // e메일 도메인
+    // console.log('ownrZip', ownrZip);    // 우편번호
+    // console.log('ownrAddr1', ownrAddr1);    // 주소1
+    // console.log('ownrAddr2', ownrAddr2);    // 주소2
+    // console.log('ownrBrno', ownrBrno);    // 사업자등록번호
+    // console.log('txblRcvYn', txblRcvYn);    // 세금 납부일
+    // console.log('purDesc', purDesc);    // 특이사항
+    // console.log('parkingCd', parkingCd);    // 주차위치 코드
+    // console.log('parkingLocationDesc', parkingLocationDesc);    // 주차위치 설명
+    // console.log('fctCndcYn', fctCndcYn);    // 사실확인서
+    // console.log('attachedFiles', attachedFiles);    // 관련 서류 첨부
+    // console.log('parkKeyNo', parkKeyNo);    // Key번호
 
     // 매입딜러
-    if(!dealerId) {
-      alert('매입딜러를 선택해주세요.');
-      return;
-    }
+    // if(!dealerId) {
+    //   alert('매입딜러를 선택해주세요.');
+    //   return;
+    // }
 
-    // 매입금액
-    if(!purAmt || purAmt === '0') {
-      alert('매입금액을 입력해주세요.');
-      return;
-    }
+    // // 매입금액
+    // if(!purAmt || purAmt === '0') {
+    //   alert('매입금액을 입력해주세요.');
+    //   return;
+    // }
 
-    // 매입일
-    if(!carPurDt) {
-      alert('매입일을 선택해주세요.');
-      return;
-    }
+    // // 매입일
+    // if(!carPurDt) {
+    //   alert('매입일을 선택해주세요.');
+    //   return;
+    // }
 
-    // 상사매입비
-    if(!agentPurCst) {
-      alert('상사매입비를 입력해주세요.');
-      return;
-    }
+    // // 상사매입비
+    // if(!agentPurCst) {
+    //   alert('상사매입비를 입력해주세요.');
+    //   return;
+    // }
 
-    // 취득세
-    if(!gainTax) {
-      alert('취득세를 선택해주세요.');
-      return;
-    }
+    // // 취득세
+    // if(!gainTax) {
+    //   alert('취득세를 선택해주세요.');
+    //   return;
+    // }
 
-    // 차량 유형
-    if(!carKndCd) {
-      alert('차량 유형을 선택해주세요.');
-      return;
-    }
+    // // 차량 유형
+    // if(!carKndCd) {
+    //   alert('차량 유형을 선택해주세요.');
+    //   return;
+    // }
     
-    // 차량명
-    if(!carNm) {
-      alert('차량명을 입력해주세요.');
-      return;
-    }
+    // // 차량명
+    // if(!carNm) {
+    //   alert('차량명을 입력해주세요.');
+    //   return;
+    // }
 
-    // 차량번호(매입후)
-    if(!carNo) {
-      alert('차량번호(매입후)를 입력해주세요.');
-      return;
-    }
+    // // 차량번호(매입후)
+    // if(!carNo) {
+    //   alert('차량번호(매입후)를 입력해주세요.');
+    //   return;
+    // }
 
-    // 고객명
-    if(!ownrNm) {
-      alert('매도자/전소유자명을 입력해주세요.');
-      return;
-    }
+    // // 고객명
+    // if(!ownrNm) {
+    //   alert('매도자/전소유자명을 입력해주세요.');
+    //   return;
+    // }
 
-    // 고객구분
-    if(!ownrTpCd) {
-      alert('고객구분을 선택해주세요.');
-      return;
-    }
+    // // 고객구분
+    // if(!ownrTpCd) {
+    //   alert('고객구분을 선택해주세요.');
+    //   return;
+    // }
 
-    // 증빙종류
-    if(!evdcCd) {
-      alert('증빙종류를 선택해주세요.');
-      return;
-    }
+    // // 증빙종류
+    // if(!evdcCd) {
+    //   alert('증빙종류를 선택해주세요.');
+    //   return;
+    // }
 
-    // 주민(법인)등록번호
-    if(ownrSsn) {
+    // // 주민(법인)등록번호
+    // if(ownrSsn) {
 
-      // 주민번호 체크 
-      if(!isValidResidentNumber(ownrSsn) && ownrTpCd === OWNER_TYPE.INDIVIDUAL) {
-        alert('주민등록번호를 확인해주세요.');
-        return;
-      } else if(!isValidCorporateNumber(ownrSsn) && ownrTpCd === OWNER_TYPE.CORPORATION) {
-        alert('법인등록번호를 확인해주세요.');
-        return;
-      }
+    //   // 주민번호 체크 
+    //   if(!isValidResidentNumber(ownrSsn) && ownrTpCd === OWNER_TYPE.INDIVIDUAL) {
+    //     alert('주민등록번호를 확인해주세요.');
+    //     return;
+    //   } else if(!isValidCorporateNumber(ownrSsn) && ownrTpCd === OWNER_TYPE.CORPORATION) {
+    //     alert('법인등록번호를 확인해주세요.');
+    //     return;
+    //   }
 
-    }
+    // }
 
-    // 사업자번호
-    if(ownrBrno) {
-      if(!checkBizID(ownrBrno)) {
-        alert('사업자등록번호를 확인해주세요.');
-        return;
-      }
-    }
+    // // 사업자번호
+    // if(ownrBrno) {
+    //   if(!checkBizID(ownrBrno)) {
+    //     alert('사업자등록번호를 확인해주세요.');
+    //     return;
+    //   }
+    // }
 
     const formValues = {
-      carRegId : carPurDetail.CAR_REG_ID,                        // 매입차량 ID
+      carRegId : selectedCar?.CAR_REG_ID,                        // 매입차량 ID
       carAgent: session?.agentId,                                // 상사사 ID
       purAmt,                                                    // 매입금액
       purSupPrc,                                                 // 공급가액
@@ -529,13 +529,14 @@ export default function SalesRegisterPage({
       txblRcvYn,                                                 // 매입수취여부
       ctshNo,                                                     // 계약서번호
       carRegDt,                                                  // 이전일
+      buyerCustomers,                                            // 매수고객 정보
     };
 
 
     console.log('formValues', formValues);
 
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/updatePurchase`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/updateCarSel`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -1350,7 +1351,7 @@ export default function SalesRegisterPage({
         <button className="btn btn--primary" type="button" disabled>
           확인
         </button>
-        <button className="btn btn--primary" type="button">
+        <button className="btn btn--primary" type="button" onClick={handleSubmit}>
           확인
         </button>
       </div>
